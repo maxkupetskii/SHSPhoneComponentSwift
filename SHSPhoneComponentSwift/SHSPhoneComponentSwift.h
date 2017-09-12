927f3a64c589c4f312c0f3e4d44aa6012a6e75ee
Max Kupetskii <maksim_kupetskii@epam.com>
2017-09-12 10:22:52 +0300

    Initial commit

---
+++ SHSPhoneComponentSwift/SHSPhoneComponentSwift.h
@@ -0,0 +1,19 @@
+//
+//  SHSPhoneComponentSwift.h
+//  SHSPhoneComponentSwift
+//
+//  Created by Maksim Kupetskii on 12/09/2017.
+//  Copyright © 2017 Maksim Kupetskii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHSPhoneComponentSwift.
+FOUNDATION_EXPORT double SHSPhoneComponentSwiftVersionNumber;
+
+//! Project version string for SHSPhoneComponentSwift.
+FOUNDATION_EXPORT const unsigned char SHSPhoneComponentSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHSPhoneComponentSwift/PublicHeader.h>
+
+

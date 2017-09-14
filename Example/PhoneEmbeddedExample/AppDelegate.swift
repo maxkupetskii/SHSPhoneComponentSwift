613c75b69e41e572901e7302c5e23d800af7e550
Max Kupetskii <maksim_kupetskii@epam.com>
2017-09-14 15:30:43 +0300

    Example added

---
+++ Example/PhoneEmbeddedExample/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  PhoneEmbeddedExample
+//
+//  Created by Will on 20/01/15.
+//  Copyright (c) 2015 SHS. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
